1df9db57fe8e1c4bb63ebaa9e9de927835d46250
Jord Gui <jord_gui@hotmail.com>
2020-05-18 21:20:09 +1000

    completed dom method exercises

--- dom-manipulation-odin-project/myApp.js
+++ dom-manipulation-odin-project/myApp.js
@@ -6,3 +6,29 @@ content.classList.add('content');
 content.textContent = 'This is the glorious text-content!';
 
 container.appendChild(content);
+
+// exercise 1
+const redP = document.createElement('p');
+redP.style.color = 'red';
+redP.textContent = "Hey I'm red!";
+container.appendChild(redP);
+
+// exercise 2
+const blueHeading = document.createElement('h3');
+blueHeading.style.color = 'blue';
+blueHeading.textContent = "I'm a blue h3!";
+container.insertBefore(blueHeading, content);
+
+// exercise 3
+const anotherDiv = document.createElement('div');
+anotherDiv.style.backgroundColor = 'pink';
+anotherDiv.style.borderColor = 'black';
+container.appendChild(anotherDiv);
+
+const heading1 = document.createElement('h1');
+heading1.textContent = "I'm in a div";
+anotherDiv.appendChild(heading1);
+
+const anotherP = document.createElement('p');
+anotherP.textContent = "ME TOO!";
+anotherDiv.appendChild(anotherP);
