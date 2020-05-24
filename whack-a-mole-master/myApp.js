e27192d5f7e595ae32e9cadabbe233216842bd9a
Jord <jgui0003@student.monash.edu>
2020-05-24 19:15:11 +1000

    added functionality

--- whack-a-mole-master/myApp.js
+++ whack-a-mole-master/myApp.js
@@ -4,13 +4,14 @@
 
 // select all divs with class name square and call them square for JS file
 // the dot (.) is use for class names
-const square = docuemnt.querySelectorAll('.square');
+const square = document.querySelectorAll('.square');
 const mole = document.querySelectorAll('.mole');
 // find id of time left (shown using # as opposed to . for class)
 const timeLeft = document.querySelector('#time-left');
 let score = document.querySelector('#score');
 
 let result = 0;
+let currentTime = timeLeft.textContent;
 
 function randomSquare() {
     // first remove a potential mole from all squares in the grid by removing the 'mole' class from a div
@@ -21,4 +22,37 @@ function randomSquare() {
     let randomPosition = square[Math.floor(Math.random() * 9)];
     // add the class of mole to this position
     randomPosition.classList.add('mole');
+    //assign the id of the randomPosition to hitPosition for us to use later
+    hitPosition = randomPosition.id;
 }
+
+// add an event to each square to see if a mole has been hit
+square.forEach(id => {
+    id.addEventListener('mouseup', () => {
+        if(id.id === hitPosition){
+        result = result + 1;
+        score.textContent = result;
+        }
+    })
+})
+
+
+function moveMole() {
+    let timerId = null;
+    timerId = setInterval(randomSquare, 500);
+}
+
+moveMole()
+
+
+function countDown() {
+    currentTime--;
+    timeLeft.textContent = currentTime;
+
+    if(currentTime === 0 ) {
+        clearInterval(timerId)
+        alert('GAME OVER! Your final score is' + result)
+    }
+}
+
+let timerId = setInterval(countDown, 1000)
